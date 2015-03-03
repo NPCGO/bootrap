505eb41d56ae7f2c7ab9c68820a5c98704e23a67
Quang Huy <anybao4@gmail.com>
2015-03-03 22:33:54 +0700

    Add
    Web App

---
+++ schedule.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Quang Huy Website - Schedule</title>
+
+    <!-- Bootstrap -->
+    <link href='http://fonts.googleapis.com/css?family=Bree+Serif|Merriweather:400,300,300italic,400italic,700italic,700' rel='stylesheet' type='text/css'>
+    <link href="_/css/bootstrap.css" rel="stylesheet">
+    <link href="_/css/mystyles.css" rel="stylesheet">
+
+  </head>
+  <body id = "schedule">
+    <section class = "container">
+      <div class="content">
+        <section class = "main col col-lg-8">
+
+        </section><!--main-->
+        <section class = "sidebar col col-lg-4">
+
+        </section><!--sidebar-->
+      </div><!--content-->
+    </section><!--contatiner-->
+
+    <script src="_/js/bootstrap.js"></script>
+    <script src="_/js/myscript.js"></script>
+  </body>
+</html>
